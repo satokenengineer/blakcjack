29056f1de03a96e06e21d9f73bdac65c5c934af7
satokenengine <satokenengineer@gmail.com>
2022-01-31 11:18:16 +0900

    first commit

---
+++ blackjack.rb
@@ -0,0 +1,15 @@
+require_relative "message"
+require_relative "deck"
+
+class Blackjack
+  include Message
+
+  def initialize(dealer, player)
+    @dealer = dealer
+    @player = player
+  end
+
+  def start
+    start_msg
+  end
+end
